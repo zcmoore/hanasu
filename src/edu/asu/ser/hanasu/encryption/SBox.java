67c29a9c50698bb22c4fb8d4158ebda03fd1fa00
Zach Moore <zcmoore@asu.edu>
2014-03-21 10:42:22 -0700

    added constant RijndaelSBox implementation

--- src/edu/asu/ser/hanasu/encryption/SBox.java
+++ src/edu/asu/ser/hanasu/encryption/SBox.java
@@ -1,6 +1,31 @@
 package edu.asu.ser.hanasu.encryption;
 
-public class SBox
+import java.net.URL;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public abstract class SBox
 {
+	/** Contents of this box, used for substitution */
+	protected byte[] boxValues;
 	
+	public static SBox loadSBox(URL filePath)
+	{
+		throw new NotImplementedException();
+	}
+	
+	public static SBox loadSBox(String filePath)
+	{
+		throw new NotImplementedException();
+	}
+	
+	public static SBox generateSBox(GaloisField galoisField)
+	{
+		throw new NotImplementedException();
+	}
+	
+	public static SBox generateSBox(int galoisBase, int galoisExponent)
+	{
+		throw new NotImplementedException();
+	}
 }
