04b25c36110446ccfe1fdcb4adb6dbeb8f3c613f
BlackRain <nmorgan1214@gmail.com>
2014-03-25 10:21:28 -0700

    Commiting on server branch

--- src/edu/asu/ser/hanasu/server/EncryptedMessage.java
+++ src/edu/asu/ser/hanasu/server/EncryptedMessage.java
@@ -1,23 +1,36 @@
 package edu.asu.ser.hanasu.server;
 
 import java.io.*;
+import java.net.InetAddress;
 
 public class EncryptedMessage implements Serializable
 {
-	//Generated serial id
+	// Generated serial id
 	private static final long serialVersionUID = 9171419644778126526L;
+	
 	static final int CLIENTSCONNECTED = 0, MESSAGE = 1, LOGOUT = 2;
 	private int type;
+	
+	// IP to send to via server send method
+	private InetAddress sendTo;
+	
+	// Encrypted Message
 	private byte[] message;
 	
 	// constructor
-	EncryptedMessage(int type, byte[] message)
+	EncryptedMessage(int type, byte[] message, InetAddress sendTo)
 	{
 		this.type = type;
 		this.message = message;
+		this.sendTo = sendTo;
+		
 	}
 	
-	// getters
+	public InetAddress getSendTo()
+	{
+		return sendTo;
+	}
+	
 	public int getType()
 	{
 		return type;
