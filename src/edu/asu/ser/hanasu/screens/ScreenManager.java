d4f3fd7a9c2463cc44b4cfd6fe656def29c21759
Zach Moore <zcmoore@asu.edu>
2014-03-28 15:39:04 -0700

    implemented screen transitions

--- src/edu/asu/ser/hanasu/screens/ScreenManager.java
+++ src/edu/asu/ser/hanasu/screens/ScreenManager.java
@@ -1,19 +1,22 @@
 package edu.asu.ser.hanasu.screens;
 
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Image;
-import java.util.ArrayList;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import javax.swing.JPanel;
+import javax.swing.JViewport;
+import javax.swing.Timer;
 
 public class ScreenManager
 {
-	private static Image barImage;
-	private static ArrayList<Screen> screens;
-	private static Screen defaultScreen;
-	private static Screen currentScreen;
-	private static String nickName;
+	private static ScreenManager activeManager;
 	private static ClientContainer clientContainer;
+	private static Image barImage;
+	private static Timer transitionTimer;
 	
 	/**
 	 * Launch the application.
@@ -23,84 +26,100 @@ public class ScreenManager
 		EventQueue.invokeLater(new Runnable() {
 			public void run()
 			{
-				try
-				{
-					clientContainer = new ClientContainer();
-					clientContainer.setVisible(true);
-					
-				}
-				catch (Exception e)
-				{
-					e.printStackTrace();
-				}
+				ScreenManager.createManager();
 			}
 		});
 	}
 	
+	private ScreenManager()
+	{
+		if (clientContainer == null)
+		{
+			clientContainer = new ClientContainer();
+			clientContainer.setVisible(true);
+		}
+	}
+	
+	public static ScreenManager createManager()
+	{
+		ScreenManager newManager = new ScreenManager();
+		
+		if (activeManager == null)
+		{
+			activeManager = new ScreenManager();
+		}
+		
+		return newManager;
+	}
+	
 	public static void transition(Screen destination)
 	{
-		//TODO: implement screen transition code
-		throw new NotImplementedException();
+		JPanel panel = clientContainer.getInnerPane();
+		destination.setPreferredSize(new Dimension(450, 300));
+		panel.add(destination);
+		
+		// TODO: modularize
+		if (transitionTimer != null)
+			transitionTimer.stop();
+		
+		transitionTimer = new Timer(5, null);
+		transitionTimer.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				JViewport viewport = clientContainer.getScrollPane().getViewport();
+				Point position = viewport.getViewPosition();
+				position.x += 5;
+				
+				if (position.x >= 450)
+				{
+					JPanel panel = clientContainer.getInnerPane();
+					panel.remove(0);
+					
+					position.x = 0;
+					viewport.setViewPosition(position);
+					transitionTimer.stop();
+				}
+				else
+				{
+					viewport.setViewPosition(position);
+				}
+				System.out.println("tick");
+			}
+		});
+		
+		transitionTimer.start();
 	}
-
-	public static Image getBarImage()
-	{
-		return barImage;
-	}
-
-	public static void setBarImage(Image barImage)
-	{
-		ScreenManager.barImage = barImage;
-	}
-
-	public static ArrayList<Screen> getScreens()
-	{
-		return screens;
-	}
-
-	public static void setScreens(ArrayList<Screen> screens)
-	{
-		ScreenManager.screens = screens;
-	}
-
-	public static Screen getDefaultScreen()
-	{
-		return defaultScreen;
-	}
-
-	public static void setDefaultScreen(Screen defaultScreen)
-	{
-		ScreenManager.defaultScreen = defaultScreen;
-	}
-
-	public static Screen getCurrentScreen()
-	{
-		return currentScreen;
-	}
-
-	public static void setCurrentScreen(Screen currentScreen)
-	{
-		ScreenManager.currentScreen = currentScreen;
-	}
-
-	public static String getNickName()
-	{
-		return nickName;
-	}
-
-	public static void setNickName(String nickName)
-	{
-		ScreenManager.nickName = nickName;
-	}
-
+	
 	public static ClientContainer getClientContainer()
 	{
 		return clientContainer;
 	}
-
+	
 	public static void setClientContainer(ClientContainer clientContainer)
 	{
 		ScreenManager.clientContainer = clientContainer;
+	}
+	
+	public static Image getBarImage()
+	{
+		return barImage;
+	}
+	
+	public static void setBarImage(Image barImage)
+	{
+		ScreenManager.barImage = barImage;
+	}
+	
+	public static ScreenManager getActiveManager()
+	{
+		return activeManager;
+	}
+	
+	public static void setActiveManager(ScreenManager activeManager)
+	{
+		ScreenManager.activeManager = activeManager;
 	}
 	
 }
