6025269555a2d412a56449b41886fd3955ff1bf1
Zach Moore <zcmoore@asu.edu>
2014-03-29 09:42:55 -0700

    decoupled class implementations

--- src/edu/asu/ser/hanasu/screens/ClientContainer.java
+++ src/edu/asu/ser/hanasu/screens/ClientContainer.java
@@ -2,23 +2,75 @@ package edu.asu.ser.hanasu.screens;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JViewport;
+import javax.swing.Timer;
+
+import edu.asu.ser.hanasu.Singleton;
 
 @SuppressWarnings("serial")
-public class ClientContainer extends JFrame
+public class ClientContainer extends JFrame implements Singleton
 {
+	private static Timer transitionTimer = new TransitionTimer();
+	private static ClientContainer activeClient;
 	private JScrollPane scrollPane;
 	private JPanel innerPane;
+	
+	private static class TransitionTimer extends Timer
+	{
+		public TransitionTimer()
+		{
+			super(5, new TransitionTimerListener());
+		}
+	}
+	
+	private static class TransitionTimerListener implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			JViewport viewport = activeClient.scrollPane.getViewport();
+			Point position = viewport.getViewPosition();
+			position.x += 5;
+			
+			if (position.x >= 450)
+			{
+				JPanel panel = activeClient.innerPane;
+				panel.remove(0);
+				
+				position.x = 0;
+				viewport.setViewPosition(position);
+				transitionTimer.stop();
+			}
+			else
+			{
+				viewport.setViewPosition(position);
+			}
+		}
+	}
+	
+	public static ClientContainer create()
+	{
+		if (activeClient == null)
+			activeClient = new ClientContainer();
+		else
+			throw new IllegalStateException("ClientContiner is Singleton");
+		
+		return activeClient;
+	}
 	
 	/**
 	 * Create the frame.
 	 */
-	public ClientContainer()
+	private ClientContainer()
 	{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450 + 16, 300 + 38);
@@ -27,8 +79,10 @@ public class ClientContainer extends JFrame
 		innerPane.setLayout(new GridLayout(1, 0, 0, 0));
 		
 		scrollPane = new JScrollPane(innerPane);
-		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+		scrollPane
+				.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane
+				.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
 		setContentPane(scrollPane);
 		
 		MainScreen mainScreen = new MainScreen();
@@ -36,22 +90,26 @@ public class ClientContainer extends JFrame
 		innerPane.add(mainScreen);
 		
 		this.addComponentListener(new ComponentAdapter() {
-            public void componentResized(ComponentEvent e) {
-            	e.getComponent().getHeight();
-            }
-        });
+			public void componentResized(ComponentEvent e)
+			{
+				e.getComponent().getHeight();
+			}
+		});
+		
+		this.setVisible(true);
 	}
 	
-	public JScrollPane getScrollPane()
+	public static void transition(JPanel destination)
 	{
-		return scrollPane;
+		JPanel panel = activeClient.innerPane;
+		destination.setPreferredSize(new Dimension(450, 300));
+		panel.add(destination);
+		
+		transitionTimer.start();
 	}
-
-	public JPanel getInnerPane()
+	
+	public static ClientContainer getActiveClient()
 	{
-		return innerPane;
+		return activeClient;
 	}
-	
-	
-	
 }
--- src/edu/asu/ser/hanasu/screens/ScreenManager.java
+++ src/edu/asu/ser/hanasu/screens/ScreenManager.java
@@ -1,82 +1,19 @@
 package edu.asu.ser.hanasu.screens;
 
-import java.awt.Dimension;
-import java.awt.EventQueue;
 import java.awt.Image;
-import java.awt.Point;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JPanel;
-import javax.swing.JViewport;
-import javax.swing.Timer;
 
 public class ScreenManager
 {
-	private static Timer transitionTimer = new TransitionTimer();
 	private static ScreenManager activeManager;
-	private static ClientContainer clientContainer;
 	private static Image barImage;
 	
 	private MainScreen mainScreen;
 	private ChannelScreen channelScreen;
-	//TODO: add chat screen
 	
-	@SuppressWarnings("serial")
-	private static class TransitionTimer extends Timer
-	{
-		public TransitionTimer()
-		{
-			super(5, new TransitionTimerListener());
-		}
-		
-	}
-	
-	private static class TransitionTimerListener implements ActionListener
-	{
-		@Override
-		public void actionPerformed(ActionEvent e)
-		{
-			JViewport viewport = clientContainer.getScrollPane().getViewport();
-			Point position = viewport.getViewPosition();
-			position.x += 5;
-			
-			if (position.x >= 450)
-			{
-				JPanel panel = clientContainer.getInnerPane();
-				panel.remove(0);
-				
-				position.x = 0;
-				viewport.setViewPosition(position);
-				transitionTimer.stop();
-			}
-			else
-			{
-				viewport.setViewPosition(position);
-			}
-		}
-	}
-	
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args)
-	{
-		EventQueue.invokeLater(new Runnable() {
-			public void run()
-			{
-				ScreenManager.createManager();
-			}
-		});
-	}
+	// TODO: add chat screen
 	
 	private ScreenManager()
 	{
-		if (clientContainer == null)
-		{
-			clientContainer = new ClientContainer();
-			clientContainer.setVisible(true);
-		}
 	}
 	
 	public static ScreenManager createManager()
@@ -84,30 +21,14 @@ public class ScreenManager
 		ScreenManager newManager = new ScreenManager();
 		
 		if (activeManager == null)
-		{
 			activeManager = newManager;
-		}
 		
 		return newManager;
 	}
 	
 	public static void transition(Screen destination)
 	{
-		JPanel panel = clientContainer.getInnerPane();
-		destination.setPreferredSize(new Dimension(450, 300));
-		panel.add(destination);
-		
-		transitionTimer.start();
-	}
-	
-	public static ClientContainer getClientContainer()
-	{
-		return clientContainer;
-	}
-	
-	public static void setClientContainer(ClientContainer clientContainer)
-	{
-		ScreenManager.clientContainer = clientContainer;
+		ClientContainer.transition(destination);
 	}
 	
 	public static Image getBarImage()
@@ -129,22 +50,22 @@ public class ScreenManager
 	{
 		ScreenManager.activeManager = this;
 	}
-
+	
 	public MainScreen getMainScreen()
 	{
 		return mainScreen;
 	}
-
+	
 	public void setMainScreen(MainScreen mainScreen)
 	{
 		this.mainScreen = mainScreen;
 	}
-
+	
 	public ChannelScreen getChannelScreen()
 	{
 		return channelScreen;
 	}
-
+	
 	public void setChannelScreen(ChannelScreen channelScreen)
 	{
 		this.channelScreen = channelScreen;
