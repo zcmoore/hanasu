3f5f07f33eee95863f12df0c14c55479c468c599
Zach Moore <zcmoore@asu.edu>
2014-04-09 20:43:16 -0700

    documentation

--- src/edu/asu/ser/hanasu/screens/AlphaPane.java
+++ src/edu/asu/ser/hanasu/screens/AlphaPane.java
@@ -5,31 +5,91 @@ import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
+// TODO: add component transparency support
+// TODO: add image support
+/**
+ * Panel to serve as a semi-transparent overlay. As of version 0.1, only solid
+ * colours are supported. The opacity of components that are added to a panel of
+ * this type are not controlled or guaranteed by this class.
+ * 
+ * @author Moore, Zachary
+ * @version 0.1
+ */
 @SuppressWarnings("serial")
 public class AlphaPane extends JPanel
 {
-	private Color colour;
-	
+	/**
+	 * Constructor. Sets the background colour (i.e. overlay colour) of this
+	 * panel to the specified colour. The default alpha value is 1.
+	 * 
+	 * @param colour
+	 */
 	public AlphaPane(Color colour)
 	{
-		this.colour = colour;
-		setOpaque(false);
+		super.setBackground(colour);
+		super.setOpaque(false);
+		setAlpha(1);
 	}
 	
+	/**
+	 * Constructor. Sets the background colour and alpha value of this panel.
+	 * 
+	 * @param colour Desired background (i.e. overlay) colour.
+	 * @param alpha Desired alpha value.
+	 */
+	public AlphaPane(Color colour, float alpha)
+	{
+		this(colour);
+		setAlpha(alpha);
+	}
+	
+	/*
+	 * (non-Javadoc) Override to set the alpha value to either 1 or 0. This
+	 * panel's isOpaque value will always remain false, so to not sho the actual
+	 * panel.
+	 * 
+	 * @see javax.swing.JComponent#setOpaque(boolean)
+	 */
+	public void setOpaque(boolean isOpaque)
+	{
+		if (isOpaque)
+			setAlpha(1);
+		else
+			setAlpha(0);
+	}
+	
+	/*
+	 * (non-Javadoc) Paints this component using the protocol specified by
+	 * JPanel, and then paints a transparent overlay that corresponds to this
+	 * panels background colour.
+	 * 
+	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+	 */
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
 		
-		g.setColor(colour);
+		g.setColor(super.getBackground());
 		g.fillRect(0, 0, getWidth(), getHeight());
 	}
 	
-	public void setOpacity(float opacity)
+	/**
+	 * Sets the alpha value for this panel.
+	 * 
+	 * Note: as of version 0.1, the specified alpha will only affect the
+	 * background colour of this panel. Any components within this container
+	 * will not be affected.
+	 * 
+	 * @param alpha
+	 */
+	public void setAlpha(float alpha)
 	{
-		 int red = colour.getRed();
-		 int green = colour.getGreen();
-		 int blue = colour.getBlue();
+		Color colour = super.getBackground();
+		int red = colour.getRed();
+		int green = colour.getGreen();
+		int blue = colour.getBlue();
 		
-		colour = new Color(red, green, blue, opacity);
+		colour = new Color(red, green, blue, alpha);
+		super.setBackground(colour);
 	}
 }
